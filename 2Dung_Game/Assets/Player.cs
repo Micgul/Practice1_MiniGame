ebba96ae3210723a3212b5a9eee90de925acda82
JeongHyeon <52498678+Micgul@users.noreply.github.com>
2020-12-06 17:53:24 +0900

    2Dung_Game
    final

---
+++ 2Dung_Game/Assets/Player.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //Gordo의 애니메이터
+    public Animator animator;
+    public float moveSpeed = 10; //기본값 초당 10초
+
+    private void Awake()
+    {
+        animator.SetTrigger("Idle_01");
+    }
+    private void Update()
+    {
+        Move();
+        Rotate();
+    }
+
+    private void Rotate()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            animator.transform.localScale = new Vector3(0.25f, 0.25f, 1f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            animator.transform.localScale = new Vector3(-0.25f, 0.25f, 1f);
+        }
+    }
+    private void Move()
+    {
+        animator.ResetTrigger("Run_01");
+        animator.ResetTrigger("Idle_01");
+
+        if (Input.GetKey(KeyCode.LeftArrow) == true)
+        {
+            transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+            animator.SetTrigger("Run_01");
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) == true)
+        {
+            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+            animator.SetTrigger("Run_01");
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) == false && Input.GetKey(KeyCode.RightArrow) == false)
+        {
+            animator.SetTrigger("Idle_01");
+        }
+    }
+}
---
+++ 2Dung_Game/Assets/SpawnManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public Transform startSpawnPoint, endSpawnPoint;
+    public GameObject dungPrefab;
+
+    private float width, height;
+
+    public float startInterval = 3.0f;
+    public float addIntervalTime = 5f;
+    public float addStep = 0.1f;
+
+    public int currentSpawnCount = 1;
+    public float addSpawnIntervalTime = 3f;
+
+    public void Awake()
+    { 
+        // 1. 영역의 넓이
+        // 2. 영역의 높이
+        // 3. 생성 코루틴 실행
+        width = endSpawnPoint.position.x - startSpawnPoint.position.x;
+        height = endSpawnPoint.position.y - startSpawnPoint.position.y;
+
+        StartCoroutine(Spawn());
+        StartCoroutine(balCoroutine());
+        StartCoroutine(balCountCoroutine());
+    }
+     
+    IEnumerator balCoroutine()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(addIntervalTime);
+            startInterval -= addStep; //간격이 줄어들면서 
+            startInterval = Mathf.Max(startInterval, 0.1f); //최소값 고정
+        }
+    }
+
+    IEnumerator balCountCoroutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(addIntervalTime);
+            currentSpawnCount += 1;
+        }
+    }
+
+    IEnumerator Spawn()
+    {
+        while (true)
+        {
+            for (int i = 0; i < currentSpawnCount; i++)
+            {
+                Vector3 randomPoint = new Vector3(Random.Range(0f, width), Random.Range(0f, height));
+                Vector3 spawnPoint = startSpawnPoint.transform.position + randomPoint;
+
+                GameObject obj = Instantiate(dungPrefab);
+                obj.transform.position = spawnPoint;
+                yield return new WaitForSeconds(Random.Range(0.1f, 0.5f));
+            }
+            yield return new WaitForSeconds(startInterval);
+        }
+    }
+}
